91e6e6cb6741435a87bddfdc325dccc5ae925a6c
Albar Moerhamsa <moerhamsa@gmail.com>
2024-02-19 12:18:49 +0700

    feat: implement register account

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -1,11 +1,12 @@
-import React from "react";
-import {
-    EnvelopeIcon,
-    LockClosedIcon,
-    AtSymbolIcon,
-    UserIcon,
-    PhoneIcon,
-} from "@heroicons/react/24/outline";
+'use client'
+import React, { useMemo } from "react";
+import { useRouter } from "next/navigation";
+import useCustomMutation from "@/hooks/useCustomMutation";
+
+import { SubmitHandler, useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { loginAccount, registerAccount } from "@/helpers/auth_server";
+import * as yup from "yup";
 import Link from "next/link";
 
 const RegisterFormsInput = [
@@ -41,7 +42,7 @@ const RegisterFormsInput = [
 
     },
     {
-        key: "password_match",
+        key: "confrim_password",
         label: "Konfrimasi Password",
         type: "password",
         placeholder: "Konfirmasi Password",
@@ -49,38 +50,86 @@ const RegisterFormsInput = [
     },
 ];
 
-const SignUp = () => {
-    return (
-        <div className="flex justify-center p-10 mt-40 w-screen">
-            <div className="flex justify-center items-center w-full">
-                <article className="flex flex-col space-y-4 justify-center w-2/6 p-10 bg-gray-100">
-                    <h1 className="text-xl font-semibold">Buat Akun</h1>
-                    <form action="" className="grid grid-cols-1 gap-5">
-                        {RegisterFormsInput.map((input) => (
-                            <section key={input.key} className="relative">
-                                <input
-                                    name={input.key}
-                                    type={input.type}
-                                    placeholder={input.placeholder}
-                                    className="w-full h-10 p-3 placeholder-gray-300 text-black text-sm rounded-sm border-none ring-1 ring-gray-200"
-                                />
-                            </section>
-                        ))}
+type Inputs = {
+    first_name: string;
+    last_name: string;
+    email: string;
+    phone_number: string;
+    password: string;
+    confrim_password: string;
+};
 
-                        <button className="w-full h-12 rounded-sm bg-gray-200 hover:bg-slate-400">
-                            <Link href={"/"} className="">
-                                Buat Akun
-                            </Link>
-                        </button>
-                    </form>
-                    <h1 className="flex justify-center text-sm">
-                        Sudah punya akun ?
-                        <Link href={"/sign-in"} className="font-semibold ml-1">
-                            Login
+const schema = yup
+    .object({
+        first_name: yup.string().required("Masukan nama depan anda"),
+        last_name: yup.string().required("Masukan nama depan anda"),
+        email: yup
+            .string()
+            .email("Not a valid email")
+            .required("Harap isi email"),
+        phone_number: yup.string().required("Masukan nomor telepon (WA)"),
+        password: yup.string().required("Masukan password"),
+        confrim_password: yup.string().required("Konfrimasi password password"),
+    })
+    .required();
+
+
+const SignUp = () => {
+    const router = useRouter();
+    const {
+        register,
+        handleSubmit,
+        watch,
+        formState: { errors, isValid },
+    } = useForm<Inputs>({
+        resolver: yupResolver(schema),
+        mode: "all",
+    });
+    const { mutation } = useCustomMutation(registerAccount as any);
+
+    const onSubmit: SubmitHandler<Inputs> = async (data) => {
+        try {
+            const resp = await mutation.mutateAsync(data);
+            if (resp.status === 200) {
+                router.push("/sign-in");
+            }
+        } catch (error) {
+            console.error(
+                "An error occurred while submitting the form:",
+                error
+            );
+        }
+    };
+
+    return (
+        <div className="flex flex-col justify-center items-center w-full h-screen mt-20">
+            <section className="w-11/12 md:w-1/3 bg-gray-100 p-5 space-y-2">
+                <h1 className="text-xl font-semibold">Buat Akun</h1>
+                <form onSubmit={handleSubmit(onSubmit)} className="grid grid-cols-1 gap-5">
+                    {RegisterFormsInput.map((input: any) => (
+                        <section key={input.key} className="relative">
+                            <input
+                                id={input.key}
+                                {...register(input?.key, { required: true })}
+                                type={input.type}
+                                placeholder={input.placeholder}
+                                className="w-full h-10 px-3 placeholder-gray-300 text-black rounded-sm border-none ring-1 ring-gray-200"
+                            />
+                        </section>
+                    ))}
+                    <button className="w-full h-12 rounded-sm bg-gray-200 hover:bg-slate-400">
+                        <Link href={"/"} className="">
+                            Buat Akun
                         </Link>
-                    </h1>
-                </article>
-            </div>
+                    </button>
+                </form>
+                <h1 className="flex justify-center text-sm">
+                    Sudah punya akun ?
+                    <Link href={"/sign-in"} className="font-semibold ml-1">
+                        Login
+                    </Link>
+                </h1>
+            </section>
         </div>
     );
 };
--- src/helpers/auth_server.ts
+++ src/helpers/auth_server.ts
@@ -16,6 +16,24 @@ export const loginAccount = async (payload: {
 };
 
 
+export const registerAccount = async (payload: {
+    first_name: string;
+    last_name: string;
+    email: string;
+    phone_number: string;
+    password: string;
+    confrim_password: string;
+}) => {
+    try {
+        const response = await axios.post("/api/auth/register", payload);
+        return response;
+    } catch (error) {
+        const axiosError = error as AxiosError;
+        const responseData = axiosError.response?.data;
+        return responseData;
+    }
+};
+
 export const logoutAccount = async () => {
     try {
         const response = await axios.post('/api/auth/logout', {});
